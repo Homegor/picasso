eaf3b1c8bf9a5296bb24d7cf1b833d7a12d2cdb1
homeg <homegor14@gmail.com>
2024-03-28 19:10:55 +0700

    dell

--- src/shared/typography/index.ts
+++
@@ -1,5 +0,0 @@
-export {IdesItem} from './idesItem'
-export {BodyItem} from './bodyItem'
-export {TitleItem} from './titleItem'
-export {NotFound} from "./notFound"
-export {ToLink} from "./toLink"
--- src/shared/typography/notFound/notFound.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export const NotFound = () => {
-    return <p className={"notFound"}>Not Found</p>
-};
