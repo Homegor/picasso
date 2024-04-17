6870a68d999ba5f1977c8ae7f7e9a22f5de6fdb2
homeg <homegor14@gmail.com>
2024-04-18 04:07:32 +0700

    fix

--- src/entities/post/postsPageHome.tsx
+++ src/entities/post/postsPageHome.tsx
@@ -1,48 +1,9 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import {useGetPostQuery} from "../../app/store/slice/slice";
 import {BodyItem, NotFound, IdesItem, PostContent, TitleItem, ToLink} from "../../shared";
 
 export const PostsPageHome = () => {
-    const [currentPostStart,setCurrentPostStart]=useState(0)
-
-    const {data, isLoading, isError, isSuccess} = useGetPostQuery({limit:30, start:currentPostStart});
-    const [isMyFetching,setIsFetchingDown]=useState(false)
-    const [isMyFetchingUp,setIsMyFetchingUp]=useState(false)
-
-    useEffect(()=>{
-        if(isMyFetching)
-        {
-            setCurrentPostStart(prev=>{
-                return prev<93?prev+1:prev
-            })
-            setIsFetchingDown(false)
-        }
-    },[isMyFetching])
-    useEffect(()=>{
-        if(isMyFetchingUp)
-        {
-            setCurrentPostStart(prev=>{
-                return prev>0?prev-1:prev
-            })
-            setIsMyFetchingUp(false)
-        }
-    },[isMyFetchingUp])
-    useEffect(()=>{
-        return ()=>{
-            document.addEventListener('scroll',scrollHandler)
-        }
-    },[])
-    const scrollHandler=(e:any):void=>{
-        if(e.target.documentElement.scrollTop<50)
-        {
-            setIsMyFetchingUp(true)
-        }
-        if(e.target.documentElement.scrollHeight-e.target.documentElement.scrollTop-window.innerHeight<50)
-        {
-            setIsFetchingDown(true)
-            window.scrollTo(0,(e.target.documentElement.scrollHeight + e.target.documentElement.scrollTop));
-        }
-    }
+    const {data, isLoading, isError, isSuccess} = useGetPostQuery({limit:30, start:0});
 
     if (isLoading) {
         return <h1>Lading...</h1>
